ec4de249a53e66be7ceb99a06305cbe6cad2eb3b
Keval <keval.langalia@live.com>
2022-03-19 21:15:55 -0400

    navigation & databinding setup

--- app/src/main/java/com/thekeval/gifexplorer/MainActivity.kt
+++ app/src/main/java/com/thekeval/gifexplorer/MainActivity.kt
@@ -2,10 +2,12 @@ package com.thekeval.gifexplorer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil.setContentView
+import com.thekeval.gifexplorer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
